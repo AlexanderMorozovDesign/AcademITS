c6b5c2c373a93385da27d03c3af2faff62ff900a
Morozov Alexander <shtoss23@gmail.com>
2020-02-25 11:23:38 +0700

    range new class

---
+++ CourseTask/Range/Range.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public static Range Intersection(Range firstRange, Range secondRange)
+        {
+            double[] doubleArray = { firstRange.From, firstRange.To, secondRange.From, secondRange.To };
+            Array.Sort(doubleArray);
+
+            double point = (doubleArray[1] + doubleArray[2]) / 2;
+
+            if (firstRange.IsInside(point))
+            {
+                return new Range(doubleArray[1], doubleArray[2]);
+            }
+
+            return null;
+        }
+
+        public static Range[] Union(Range firstRange, Range secondRange)
+        {
+            Range[] unionRange;
+
+            double[] doubleArray = { firstRange.From, firstRange.To, secondRange.From, secondRange.To };
+            Array.Sort(doubleArray);
+
+            double point = (doubleArray[1] + doubleArray[2]) / 2;
+
+            if (firstRange.IsInside(point))
+            {
+                unionRange = new Range[1];
+                unionRange[0] = new Range(doubleArray[0], doubleArray[3]);
+
+            }
+            else
+            {
+                unionRange = new Range[2];
+                unionRange[0] = new Range(doubleArray[0], doubleArray[1]);
+                unionRange[1] = new Range(doubleArray[2], doubleArray[3]);
+            }
+
+            return unionRange;
+        }
+
+        public static Range[] Difference(Range firstRange, Range secondRange)
+        {
+            Range[] differenceRange;
+
+            double[] doubleArray = { firstRange.From, firstRange.To, secondRange.From, secondRange.To };
+            Array.Sort(doubleArray);
+
+            double point = (doubleArray[1] + doubleArray[2]) / 2;
+
+            if (!firstRange.IsInside(point))
+            {
+                differenceRange = new Range[1];
+                differenceRange[0] = new Range(firstRange.From, firstRange.To);
+
+                return differenceRange;
+            }
+
+            if (firstRange.From == doubleArray[0] && firstRange.To == doubleArray[3])
+            {
+                differenceRange = new Range[2];
+                differenceRange[0] = new Range(firstRange.From, secondRange.From);
+                differenceRange[1] = new Range(secondRange.To, firstRange.To);
+
+                return differenceRange;
+            }
+
+            if (secondRange.From == doubleArray[0] && secondRange.To == doubleArray[3])
+            {
+                return null;
+            }
+
+            if (firstRange.From == doubleArray[0])
+            {
+                differenceRange = new Range[1];
+                differenceRange[0] = new Range(firstRange.From, secondRange.From);
+            }
+            else
+            {
+                differenceRange = new Range[1];
+                differenceRange[0] = new Range(secondRange.To, firstRange.To);
+            }
+
+            return differenceRange;
+        }
+    }
+}
---
+++ CourseTask/Range/RangeNew.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class RangeNew
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два вещественных числа для первого числового диапазона");
+
+            Console.Write("Начало диапазона = ");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Конец диапазона = ");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Range firstRange = new Range(from, to);
+
+            Console.WriteLine("Введите два вещественных числа для второго числового диапазона");
+
+            Console.Write("Начало диапазона = ");
+            from = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Конец диапазона = ");
+            to = Convert.ToDouble(Console.ReadLine());
+
+            Range secondRange = new Range(from, to);
+
+            Range intersection = Range.Intersection(firstRange, secondRange);
+            Range[] union = Range.Union(firstRange, secondRange);
+            Range[] difference = Range.Difference(firstRange, secondRange);
+
+            if (intersection != null)
+            {
+                Console.WriteLine("Пересечение первого и второго дипазона: {0} - {1}", intersection.From, intersection.To);
+            }
+            else
+            {
+                Console.WriteLine("Пересечение первого и второго дипазона отсутсвует");
+            }
+
+            Console.WriteLine("Объединение первого и второго дипазона: {0} - {1}", union[0].From, union[0].To);
+
+            if (union.Length > 1)
+            {
+                Console.WriteLine("                                        {0} - {1}", union[1].From, union[1].To);
+            }
+
+            if (difference != null)
+            {
+                Console.WriteLine("Разность первого и второго дипазона: {0} - {1}", difference[0].From, difference[0].To);
+
+                if (difference.Length > 1)
+                {
+                    Console.WriteLine("                                     {0} - {1}", difference[1].From, difference[1].To);
+                }
+            }
+            else
+            {
+                Console.WriteLine("разность первого и второго дипазона: нулевая");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
